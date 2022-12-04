995d12ce8f51b7c7e7e7390fb37aad82a2f5b089
HARSH289CHAUDHARY <114799920+HARSH289CHAUDHARY@users.noreply.github.com>
2022-12-04 23:17:44 +0530

    Add files via upload

---
+++ STRINGS 2.py
@@ -0,0 +1,17 @@
+a = "TAKI TAKI RUMBA BELLA CIAO BELLA CIAO"
+print(a[5:26])     #BY INDEXING YOU CAN SELECT THE SPECIFIC LETTERS.
+print(a.lower())   #lowercase the given input letters.
+print(a.upper())   #uppercase the given input letters.
+print(a.strip())   #REMOVES THE SPACE IN SATRTING AND IN ENDING.
+print(a.replace("RUMBA", "SAYONARA")) #REPLACES A WORD WITH ANOTHER WORD. 
+
+
+                                            #LEARN TABLE OF PYTHON STRING METHOD FROM GEEKSFORGEEKS.ORG
+
+
+a = "HARSH"
+length=len(a)
+b=int(length/2)
+print(a[0])
+print(a[b])
+print(a[-1])
---
+++ STRINGS.py
@@ -0,0 +1,5 @@
+quote="JUST GIVE THE BEST ,AND FUCK THE REST"
+if "BEST" in quote: 
+    print ("OOHAAYO")
+else:
+    print("BAKA")
